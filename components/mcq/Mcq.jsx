7f8cfb361aa5a8e0a7feaed62ef171e0de8b1a0c
Amul Sharma <sharmaamul64@gmail.com>
2024-05-03 17:13:23 +0530

    MCQ Component added

--- components/mcq/Mcq.jsx
+++ components/mcq/Mcq.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
 
-const Mcq = () => {
+const MCQ = ({ question, options, onSelect}) => {
+    // console.log(question, options, onSelect);
+    console.log("mcq render");
   return (
-    <div>Mcq</div>
-  )
-}
+    <div className="bg-white shadow-md rounded-md p-6 m-0">
+      {/* Question */}
+      <h2 className="text-lg font-semibold mb-4 col text-black">{question}</h2>
+      {/* Options */}
+      <div>
+        {options.map((option, index) => (
+          <div key={index} className="flex items-center mb-2">
+            <input
+              type="radio"
+              id={`option-${index}`}   
+              name="mcq-option"
+              value={option}
+              onChange={() => onSelect(option)}
+              className="mr-2"
+            />
+            <label htmlFor={`option-${index}`} className="text-black">{option}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Mcq
+export default MCQ;
