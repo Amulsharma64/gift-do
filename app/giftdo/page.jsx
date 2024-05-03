22e3453a94d244c29b9975549854459a496bec6b
Amul Sharma <sharmaamul64@gmail.com>
2024-05-04 01:47:45 +0530

    Error and loading pages added

--- app/giftdo/page.jsx
+++ app/giftdo/page.jsx
@@ -4,7 +4,7 @@ import McqContainer from "../../components/mcq/McqContainer";
 export const page = () => {
     return (
         <main className="flex min-h-screen flex-col items-center p-10">
-            <h1 className="text-4xl font-bold mb-5">Gift Dona</h1>
+            <h1 className="text-4xl font-bold mb-5">Gift Dona Page</h1>
             <McqContainer/>
         </main>
     );
